b966cedd7587c2aa0e24bb7b0486b82cea5f8187
Asken <3917HEfPpQGD>
2013-05-16 17:34:53 +0200

    Commented and created tests for Common.util.Url

--- common/test-common.js
+++ common/test-common.js
@@ -18,7 +18,45 @@ test('Common.util.Url', function() {
     ok(!K.isEmpty(Common.util.Url), 'The loader is configured correctly to test the Url class.');
 
     // Parse current url
-    url = K.create('Common.util.Url').parseUri();
-    ok()
-    K.log(url);
+    url = K.create('Common.util.Url', {
+    });
+
+    ok(url.url === window.location, 'Providing an empty string will use the window.location to parse the location');
+
+    // Parse current url
+    url = K.create('Common.util.Url', {
+        url: 'http://username:password@www.example.com:8080/path1/path2/index.html?q1=1&q2=2#anchor'
+    });
+
+    ok(url.source === 'http://username:password@www.example.com:8080/path1/path2/index.html?q1=1&q2=2#anchor', 'Full url source');
+    ok(url.protocol === 'http', 'Protocol is http');
+    ok(url.authority === 'username:password@www.example.com:8080', 'Authority is set');
+    ok(url.userInfo === 'username:password', 'UserInfo is set');
+    ok(url.user === 'username', 'Username is username');
+    ok(url.password === 'password', 'Password is password');
+    ok(url.host === 'www.example.com', 'Host is www.example.com');
+    ok(url.port === '8080', 'Port is set to 8080');
+    ok(url.relative === '/path1/path2/index.html?q1=1&q2=2#anchor', 'Relative path is set');
+    ok(url.path === '/path1/path2/index.html', 'Path is set');
+    ok(url.directory === '/path1/path2/', 'Directory is set');
+    ok(url.file === 'index.html', 'File set to index.html');
+    ok(url.query === 'q1=1&q2=2', 'The entire query string is set');
+    ok(url.anchor === 'anchor', 'Anchor is set to anchor');
+    ok(url.queryKey.q1 === '1' && url.queryKey.q2 === '2', 'Query key object parameters is set');
+
+    // Will retain url
+    url.parseUri();
+
+    ok(url.url === 'http://username:password@www.example.com:8080/path1/path2/index.html?q1=1&q2=2#anchor', 'Just running parseUri will keep the current url');
+
+    // Will change url
+    url.parseUri(window.location);
+
+    ok(url.url === window.location, 'Setting url to window.location again');
+
+    // Setting the url and then re-running the parsing
+    url.url = 'http://username:password@www.example.com:8080/path1/path2/index.html?q1=1&q2=2#anchor';
+    url.parseUri();
+
+    ok(url.url === 'http://username:password@www.example.com:8080/path1/path2/index.html?q1=1&q2=2#anchor', 'Just running parseUri will keep the current url');
 });
--- common/util/Url.js
+++ common/util/Url.js
@@ -10,11 +10,50 @@ K.define('Common.util.Url', {
     extend: 'K.Base',
 
     statics: {
-        strictMode: false,
-        key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
-        name:   "queryKey",
+        /**
+         * Parts of the location to fill
+         * @property key
+         * @type Array
+         * @default ['source','protocol','authority','userInfo','user','password','host','port','relative','path','directory','file','query','anchor']
+         * @private
+         * @static
+         */
+        key: ['source','protocol','authority','userInfo','user','password','host','port','relative','path','directory','file','query','anchor'],
+        /**
+         * The name for the object containing the query key value name value pair
+         * @property queryKeyName
+         * @type String
+         * @default queryKey
+         * @private
+         * @static
+         */
+        queryKeyName: 'queryKey',
+        /**
+         * The url query part parser regex
+         * @property parser
+         * @type RegExp
+         * @default /(?:^|&)([^&=]*)=?([^&]*)/g
+         * @private
+         * @static
+         */
         parser: /(?:^|&)([^&=]*)=?([^&]*)/g,
+        /**
+         * The strict location parser
+         * @parameter strict
+         * @type RegExp
+         * @default /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/
+         * @private
+         * @static
+         */
         strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+        /**
+         * The strict location parser
+         * @property loose
+         * @type RegExp
+         * @default /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+         * @private
+         * @static
+         */
         loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/,
 
         /**
@@ -33,23 +72,153 @@ K.define('Common.util.Url', {
     /**
      * The url to parse
      * @property {String} url The url to parse
-     *
+     * @type String
+     * @default ''
+     * @example
+     // Parse the url for the current location
+     var urlCurrentLocation = K.create('Common.util.Url');
+
+     // Parse the specified url
+     var specificUrl = K.create('Common.util.Url', {
+        url: 'http://www.example.se'
+     });
+
+     // Parse url using the static method
+     var staticMethodUrl = K.ensure('Common.util.Url').parseUri('http://www.example.se');
+
+     // Re-parse using another url is either done by setting the url property and re-running parseUri
+     specificUrl.url = 'http://www.anotherexample.com';
+     specificUrl.parseUri();
+
+     // ... or by sending the new url in, in which case the url property will be set to the new url
+     specificUrl.parseUri('http://www.anotherexample.com');
+
+     // The below is not chaning anything and will not refresh the class parameters...
+     specificUrl.parseUri();
+
+     // To refresh the class to have parsed the current location
+     urlCurrentLocation.parseUri(window.location);
      */
     url: '',
 
+    /**
+     * Determine if using strict or loose parser. Strict is following the RFC.
+     * @property strictMode
+     * @type Boolean
+     * default false
+     */
+    strictMode: false,
+
+    /**
+     * Source of the location
+     * @property source
+     * @type String
+     * @default ''
+     */
     source: '',
+
+    /**
+     * Protocol of the location
+     * @property protocol
+     * @type String
+     * @default ''
+     */
     protocol: '',
+
+    /**
+     * Authority of the location. This is the username, password, url and port sections of the url
+     * @property authority
+     * @type String
+     * @default ''
+     */
     authority: '',
+
+    /**
+     * Authority of the location. This is the username and password sections of the url
+     * @property userInfo
+     * @type String
+     * @default ''
+     */
     userInfo: '',
+
+    /**
+     * Username of the location
+     * @property user
+     * @type String
+     * @default ''
+     */
     user: '',
+
+    /**
+     * Password of the location
+     * @property password
+     * @type String
+     * @default ''
+     */
     password: '',
+
+    /**
+     * Host of the location
+     * @property host
+     * @type String
+     * @default ''
+     */
     host: '',
+
+    /**
+     * Port of the location
+     * @property port
+     * @type String
+     * @default ''
+     */
     port: '',
+
+    /**
+     * Relative path of the location
+     * @property relative
+     * @type String
+     * @default ''
+     */
     relative: '',
+
+    /**
+     * Path of the location
+     * @property path
+     * @type String
+     * @default ''
+     */
     path: '',
+
+    /**
+     * Directory of the location
+     * @property directory
+     * @type String
+     * @default ''
+     */
     directory: '',
+
+    /**
+     * File name of the location
+     * @property file
+     * @type String
+     * @default ''
+     */
     file: '',
+
+    /**
+     * Query part of the location
+     * @property query
+     * @type String
+     * @default ''
+     */
     query: '',
+
+    /**
+     * Anchor of the location
+     * @property anchor
+     * @type String
+     * @default ''
+     */
     anchor: '',
 
     init: function(config) {
@@ -61,24 +230,32 @@ K.define('Common.util.Url', {
     },
 
     /**
-     *
-     * @param url
-     * @returns {{}}
+     * Parse the url to the class parameters
+     * @method parseUri
+     * @param {String|Location} url The url to parse
+     * @returns {Common.util.Url}
+     * @chainable
      */
     parseUri: function (url) {
         var me = this,
-            statics = Common.util.Url,
-            m = statics[statics.strictMode ? "strict" : "loose"].exec(K.isEmpty(url) ? document.location : url),
-            uri = {},
-            i = 14;
+            _isEmpty = K.isEmpty
 
-        while (i--) uri[statics.key[i]] = m[i] || "";
+        me.url = _isEmpty(me.url) ? window.location : me.url;
 
-        uri[statics.name] = {};
-        uri[statics.key[12]].replace(statics.parser, function ($0, $1, $2) {
-            if ($1) uri[statics.name][$1] = $2;
+        var _url = _isEmpty(url) ? me.url : url,
+            _statics = Common.util.Url,
+            _m = _statics[me.strictMode ? 'strict' : 'loose'].exec(_url),
+            _i = 14;
+
+        me.url = _url;
+
+        while (_i--) me[_statics.key[_i]] = _m[_i] || '';
+
+        me[_statics.queryKeyName] = {};
+        me[_statics.key[12]].replace(_statics.parser, function ($0, $1, $2) {
+            if ($1) me[_statics.queryKeyName][$1] = $2;
         });
 
-        return uri;
+        return me;
     }
 });
